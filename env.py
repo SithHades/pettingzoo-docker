afd160e1100a10390a4996f5b576925d945baf3d
SithHades <l.knaack93@gmail.com>
2023-06-05 22:28:29 +0200

    Working code

--- env.py
+++ env.py
@@ -3,7 +3,7 @@ import requests
 import jsonpickle as json
 import time
 
-env = cooperative_pong_v5.parallel_env(render_mode="human")
+env = cooperative_pong_v5.parallel_env(render_mode="rgb_array")
 
 agent_urls = {
     "paddle_0": "http://agent_1:5000/select_action",
@@ -15,9 +15,9 @@ def get_action(agent, action_space):
     while True:
         try:
             resp = requests.post(agent_urls[agent], json.dumps(action_space))
-            print(resp)
+            print(json.loads(resp.text))
             resp.raise_for_status
-            return json.loads(resp.json())
+            return json.loads(resp.text)
         except requests.exceptions.RequestException:
             print(f"Couldn't connect to agent {agent}, retrying...")
             time.sleep(1)
